dc13486dc08b583b8d876d8a8ffa36ca8b92eafa
Juha Aittamaa <juha.aittamaa@teliasonera.com>
2021-02-05 21:47:50 +0200

    added replace for key

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ interface Entry {
 firebase.initializeApp({
   credential: firebase.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: "https://raftbot-ef942.firebaseio.com",
